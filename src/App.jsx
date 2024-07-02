fb9c675b66a218bf1c818794d5217d1e74129f7e
Gorthdzx <krislaksana123@gmail.com>
2024-07-02 08:24:10 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SortingVisualizer from "./components/SortingVisualizer.jsx";
+
+function App() {
+    return (
+        <div>
+            <SortingVisualizer></SortingVisualizer>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/SortingVisualizer.jsx
@@ -0,0 +1,7 @@
+import {Component} from "react";
+
+export  default class SortingVisualizer extends Component{
+    constructor(props) {
+        super(props);
+    }
+}
