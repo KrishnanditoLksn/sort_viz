f4d7c0b6eed24f8faca447e8573b9e01fc6e209e
Gorthdzx <krislaksana123@gmail.com>
2024-07-06 11:32:23 +0700

    feat : call insertionSort method

--- src/tests/SortTesting.js
+++ src/tests/SortTesting.js
@@ -1,3 +1,7 @@
+import {bubbleSort, insertionSort} from "../components/utils/SortingAlgos.js";
+// eslint-disable-next-line no-undef
+import nano from 'nano-time'
+
 export function areArrayEquals(arr, arr2) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
@@ -8,3 +12,9 @@ export function areArrayEquals(arr, arr2) {
     }
     return false
 }
+
+console.log(nano.microseconds())
+let arr = [5, 2, 4, 6, 3]
+console.log(insertionSort(arr))
+console.log(bubbleSort(arr))
+console.log(nano.microseconds())
