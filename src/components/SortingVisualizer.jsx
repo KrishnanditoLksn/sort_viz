f4c14bb6e16457707e6d7f19362f30dd2c49ff72
Gorthdzx <krislaksana123@gmail.com>
2024-09-02 16:35:53 +0700

    feat : implement merge

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import '../App.css'
 import Button from "./ui/Button.jsx";
-import {bubbleSort, insertionSort} from "./utils/SortingAlgos.js";
+import {bubbleSort, insertionSort, mergeSort} from "./utils/SortingAlgos.js";
 
 export const SortingVisualizer = () => {
     const [array, setArray] = useState([]);
@@ -29,13 +29,37 @@ export const SortingVisualizer = () => {
             }, i * 75)
         }
     }
-    
+
+    const handleMergeSortVisualizer = () => {
+        const animations = mergeSort(array)
+        // const tempAnimations = []
+        // for (let i = 0; i < animations.length; i++) {
+        //     const arrayBars = document.getElementsByClassName("array-bar");
+        //     const ifColorChange = i % 3 !== 2;
+        //     if (ifColorChange) {
+        //         const [barOneIdx, barTwoIdx] = animations[i];
+        //         const barOneStyle = arrayBars[barOneIdx].style;
+        //         const barTwoStyle = arrayBars[barTwoIdx].style;
+        //         const color = i % 3 === 0 ? "red" : "blue";
+        //         setTimeout(() => {
+        //             barOneStyle.backgroundColor = color;
+        //             barTwoStyle.backgroundColor = color;
+        //         }, i);
+        //     } else {
+        //         setTimeout(() => {
+        //             const [barOneIdx, newHeight] = animations[i];
+        //             const barOneStyle = arrayBars[barOneIdx].style;
+        //             barOneStyle.height = `${newHeight}px`;
+        //         }, i);
+        //     }
+        // }
+    }
     /*
     when the button is clicked, handleResetArray is called
      */
     const handleResetArray = () => {
         const newarray = [];
-        for (let i = 0; i < 25; i++) {
+        for (let i = 0; i < 100; i++) {
             newarray.push(randomIntegerFromInterval(1, 1000));
         }
         alert("Generating New Array.......")
@@ -49,11 +73,10 @@ export const SortingVisualizer = () => {
         <div className="container">
             <div className="sort-container">
                 <Button onClick={handleResetArray} text="Generate New Array"></Button>
-                <Button text="Merge Sort"></Button>
+                <Button onClick={handleMergeSortVisualizer} text="Merge Sort"></Button>
                 <Button text="Quick  Sort"></Button>
                 <Button onClick={handleInsertionSortVisualizer} text="Insertion Sort"></Button>
                 <Button onClick={handleBubbleSortVisualizer} text="Bubble Sort"></Button>
-                <Button text="Merge Sort"></Button>
             </div>
             <div className="array-bar-container">
                 {array.map((value, idx) => (
