c9a430b4de9fbe8ca867f92b7ca845634884a2ee
Gorthdzx <krislaksana123@gmail.com>
2024-09-02 16:34:45 +0700

    feat : test double merge sort algo

--- src/components/utils/SortingAlgos.js
+++ src/components/utils/SortingAlgos.js
@@ -33,15 +33,78 @@ export const insertionSort = (array) => {
     return animation
 }
 
-export const mergeSort = () =>{
-    
+export const mergeSort = (arr) => {
+    const animation = []
+    if (arr.length <= 1) {
+        return arr
+    }
+    const auxArray = arr.slice()
+    /*
+    call merge sort helper function
+     */
+    mergeSortHelper(
+        arr,0,arr.length - 1 ,auxArray,animation
+    )
+    return animation
 }
 
-function mergeSortHelper(){
-    
+
+function mergeSortHelper(
+    array,
+    left,
+    right,
+    auxiliaryArray,
+    animations
+) {
+    if (left === right) return [];
+    const mid = Math.floor((left + right) / 2)
+    mergeSortHelper(auxiliaryArray, left, mid, array, animations)
+    mergeSortHelper(auxiliaryArray, mid + 1, right, array, animations)
+    /*
+    call merge function
+     */
+    merge(
+        array, left, mid, right, auxiliaryArray, animations
+    )
 }
 
+function merge(
+    array, startIdx, midIdx, endIdx, tempArray, animations
+) {
+    let k = startIdx
+    let i = startIdx
+    let j = midIdx + 1
 
-function merge(arr , p , q , r){
-    let l = q- p + 1
+    while (i <= midIdx && j <= endIdx) {
+        const animation = {}
+        animation.comparison = [i, j]
+        if (
+            tempArray[i] <= tempArray
+        ) {
+            animation.swap = [k, i]
+            array[k++] = tempArray[i++]
+        } else {
+            animation.swap = [k, j]
+            array[k++] = tempArray[j++]
+        }
+    }
+    while (i <= midIdx) {
+        animations.push(
+            {
+                comparison: [i, i],
+                swap: [k, i]
+            }
+        )
+        array[k++] = tempArray[i++]
+    }
+
+    while (j <= midIdx) {
+        animations.push(
+            {
+                comparison: [j, j],
+                swap: [k, j]
+            }
+        )
+        array[k++] = tempArray[j++]
+    }
 }
