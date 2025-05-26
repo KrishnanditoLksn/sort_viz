0e0e25a43b402bca2a6f1a9c37f0095e5eba3939
KrishnanditoLksn <krislaksana123@gmail.com>
2025-05-26 11:22:52 +0700

    refactor : indentation

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,78 +1,83 @@
-import {useEffect, useState} from "react";
-import '../App.css'
+import { useEffect, useState } from "react";
+import "../App.css";
 import Button from "./ui/Button.jsx";
-import {bubbleSort, insertionSort} from "./utils/SortingAlgos.js";
+import { bubbleSort, insertionSort } from "./utils/SortingAlgos.js";
 
 export const SortingVisualizer = () => {
     const [array, setArray] = useState([]);
     /*
-    Handle a reset array when a component is mounted
-     */
+      Handle a reset array when a component is mounted
+       */
     useEffect(() => {
-        handleResetArray()
+        handleResetArray();
     }, []);
 
     const handleBubbleSortVisualizer = () => {
-        const bubbleSortIterator = bubbleSort(array)
+        const bubbleSortIterator = bubbleSort(array);
         for (let i = 0; i < bubbleSortIterator.length; i++) {
             setTimeout(() => {
-                setArray(bubbleSortIterator[i])
-            }, i)
+                setArray(bubbleSortIterator[i]);
+            }, i);
         }
-    }
+    };
 
     const handleInsertionSortVisualizer = () => {
-        const insert = insertionSort(array)
+        const insert = insertionSort(array);
         for (let i = 0; i < insert.length; i++) {
             setTimeout(() => {
-                setArray(insert[i])
-            }, i * 75)
+                setArray(insert[i]);
+            }, i * 75);
         }
-    }
-    
+    };
+
     /*
-    when the button is clicked, handleResetArray is called
-     */
+      when the button is clicked, handleResetArray is called
+       */
     const handleResetArray = () => {
         const newarray = [];
         for (let i = 0; i < 25; i++) {
             newarray.push(randomIntegerFromInterval(1, 1000));
         }
-        alert("Generating New Array.......")
-        setArray(newarray)
-    }
+        alert("Generating New Array.......");
+        setArray(newarray);
+    };
 
-    /*
-    Return a div with array bars
-     */
+    /* Return a div with array bars
+       */
     return (
         <div className="container">
             <div className="sort-container">
                 <Button onClick={handleResetArray} text="Generate New Array"></Button>
                 <Button text="Merge Sort"></Button>
                 <Button text="Quick  Sort"></Button>
-                <Button onClick={handleInsertionSortVisualizer} text="Insertion Sort"></Button>
-                <Button onClick={handleBubbleSortVisualizer} text="Bubble Sort"></Button>
+                <Button
+                    onClick={handleInsertionSortVisualizer}
+                    text="Insertion Sort"
+                ></Button>
+                <Button
+                    onClick={handleBubbleSortVisualizer}
+                    text="Bubble Sort"
+                ></Button>
                 <Button text="Merge Sort"></Button>
             </div>
             <div className="array-bar-container">
                 {array.map((value, idx) => (
-                    <div className="array-bar" key={idx} style=
-                        {
-                            {
-                                height: `${value}px`
-                            }
-                        }>
-                    </div>
+                    <div
+                        className="array-bar"
+                        key={idx}
+                        style={{
+                            height: `${value}px`,
+                        }}
+                    ></div>
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 const randomIntegerFromInterval = (min, max) => {
     /*
-    Return a random Integer between min ,  max
-     */
+      Return a random Integer between min ,  max
+       */
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
